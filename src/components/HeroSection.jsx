29fb172a1af4e0c58df76d504c19ac40b3d91f0f
NicoGelato <86999857+NicoGelato@users.noreply.github.com>
2022-01-25 00:33:30 -0300

    Migre de Material-ui v4 a v5, descargue typescript, agregre PathPrefix a gatsby-confing, agregue {index: 1} a los useStyles

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,14 +1,13 @@
 import React, { useState, useEffect } from "react";
-// Componentes Material UI
-import { makeStyles } from "@material-ui/core/styles";
-import Paper from "@material-ui/core/Paper";
-import Container from "@material-ui/core/Container";
-import Grid from "@material-ui/core/Grid";
-import Typography from "@material-ui/core/Typography";
-import Box from "@material-ui/core/Box";
-import Button from "@material-ui/core/Button";
-import Grow from "@material-ui/core/Grow";
-import { Hidden } from "@material-ui/core";
+import makeStyles from '@mui/styles/makeStyles';
+import Paper from "@mui/material/Paper";
+import Container from "@mui/material/Container";
+import Grid from "@mui/material/Grid";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import Grow from "@mui/material/Grow";
+import { Hidden } from "@mui/material";
 import { StaticImage } from "gatsby-plugin-image";
 // Iconos
 import { DiHtml5 } from "@react-icons/all-files/di/DiHtml5";
@@ -16,39 +15,44 @@ import { DiCss3 } from "@react-icons/all-files/di/DiCss3";
 import { DiJavascript1 } from "@react-icons/all-files/di/DiJavascript1";
 import { DiReact } from "@react-icons/all-files/di/DiReact";
 // Fondo
-import backgroundImage from "../images/portfolioCodingImage_2.jpeg";
+import backgroundImage from "../images/imageHeroSection.jpg";
 // Componentes locales
 import Social from "./Social";
 
 // Styles
-const useStyles = makeStyles((theme) => ({
-  section: {
-    height: "85vh",
-    position: "relative"
-  },
-  container: {
-    height: "100%",
-  },
-  contend: {
-    height: "100%",
-  },
-  backDrop: {
-    backdropFilter: "blur(3px)",
-    backgroundColor: "rgba(0,0,10,0.3)",
-    padding: "9vh",
-    borderRadius: "3px",
-  },
+const useStyles = makeStyles(
+  
+    {
+      section: {
+        height: "85vh",
+        position: "relative",
+      },
+      container: {
+        height: "100%",
+      },
+      contend: {
+        height: "100%",
+      },
+      backDrop: {
+        backdropFilter: "blur(3px)",
+        backgroundColor: "rgba(0,0,10,0.3)",
+        padding: "9vh",
+        borderRadius: "3px",
+      },
 
-  // Revisar esta parte porque no me convence mucho
-  heroImage: {
-    position: "absolute",
-    width: "100%",
-    height: "100%",
-    backgroundImage: `url(${backgroundImage})`,
-    backgroundSize: "cover",
-    backgroundPosition: "center",
-  },
-}));
+      // Revisar esta parte porque no me convence mucho
+      heroImage: {
+        position: "absolute",
+        width: "100%",
+        height: "100%",
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      },
+    },
+    { index: 1 }
+  
+);
 
 // Items Frontend
 const TechnologyItems = [
@@ -59,7 +63,7 @@ const TechnologyItems = [
 ];
 
 const HeroSection = () => {
-  const styles = useStyles();
+  const classes = useStyles();
 
   const [shouldShow, setShouldShow] = useState(false);
 
@@ -68,62 +72,60 @@ const HeroSection = () => {
   }, []);
 
   return (
-    <>
-      <Paper className={styles.section} id="Sobremí">
-        <StaticImage
-          className={styles.heroImage}
-          src="../images/portfolioCodingImage_2.jpeg"
-          alt="Imagen de fondo: netbook con codigo"
-        />
-        <Container className={styles.container} maxWidth="md">
-          <Grid
-            container
-            className={styles.contend}
-            alignItems="center"
-            justifyContent="space-between"
-            style={{ minHeight: "50vh" }}
-          >
-            <Grow in={shouldShow}>
-              <Grid item className={styles.backDrop} sm={9}>
-                <Box my={1}>
-                  <Typography componet="h1" variant="h3">
-                    Nicolás Gelato
-                  </Typography>
-                </Box>
-                <Typography componet="h2" variant="h5">
-                  Programador Frontend
+    <Paper className={classes.section}>
+      <StaticImage
+        className={classes.heroImage}
+        src="../images/imageHeroSection.jpg"
+        alt="Netbook con codigo"
+      />
+      <Container className={classes.container} maxWidth="md">
+        <Grid
+          container
+          className={classes.contend}
+          alignItems="center"
+          justifyContent="space-between"
+          style={{ minHeight: "50vh" }}
+        >
+          <Grow in={shouldShow}>
+            <Grid item className={classes.backDrop} sm={9}>
+              <Box my={1}>
+                <Typography componet="h1" variant="h3">
+                  Nicolás Gelato
                 </Typography>
-                <Box my={1}>
-                  <Typography componet="h2" variant="h6">
-                    {TechnologyItems.map((item) => item.icon)}
-                  </Typography>
-                </Box>
+              </Box>
+              <Typography componet="h2" variant="h5">
+                Programador Frontend
+              </Typography>
+              <Box my={1}>
+                <Typography componet="h2" variant="h6">
+                  {TechnologyItems.map((item) => item.icon)}
+                </Typography>
+              </Box>
 
-                <Box my={1}>
-                  <Button
-                    href="mailto:nicolasgelato@gmail.com?Subject=Estoy%20interesado%20en%20sus%20servicios%20como%20desarrollador&body=Hola%20Nico!"
-                    variant="outlined"
-                    color="primary"
-                  >
-                    Contactame
-                  </Button>
-                </Box>
-              </Grid>
-            </Grow>
-            <Hidden xsDown>
-              <Grid item>
-                <Social direction="column" />
-              </Grid>
-            </Hidden>
-            <Hidden smUp>
-              <Grid item>
-                <Social direction="row" />
-              </Grid>
-            </Hidden>
-          </Grid>
-        </Container>
-      </Paper>
-    </>
+              <Box my={1}>
+                <Button
+                  href="mailto:nicolasgelato@gmail.com?Subject=Estoy%20interesado%20en%20sus%20servicios%20como%20desarrollador&body=Hola%20Nico!"
+                  variant="outlined"
+                  color="primary"
+                >
+                  Contactame
+                </Button>
+              </Box>
+            </Grid>
+          </Grow>
+          <Hidden smDown>
+            <Grid item>
+              <Social direction="column" />
+            </Grid>
+          </Hidden>
+          <Hidden smUp>
+            <Grid item>
+              <Social direction="row" />
+            </Grid>
+          </Hidden>
+        </Grid>
+      </Container>
+    </Paper>
   );
 };
 
