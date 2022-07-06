e24a48b1d27c69b87062542834006c5395f94e1b
Ruben Nicolás Gabriel Gelato <nicolasgelato@gmail.com>
2022-07-06 19:30:35 -0300

    Agregue Helmet

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -6,10 +6,10 @@ import {
   responsiveFontSizes,
 } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
+import Helmet from "react-helmet";
 import HeroSection from "../components/HeroSection";
 import Header from "../components/Header";
 import Projects from "../components/Projects";
-
 
 let darkTheme = createTheme({
   palette: {
@@ -17,11 +17,33 @@ let darkTheme = createTheme({
   },
 });
 
-darkTheme = responsiveFontSizes(darkTheme)
+darkTheme = responsiveFontSizes(darkTheme);
 
 function indexPage() {
   return (
     <StyledEngineProvider injectFirst>
+      <Helmet>
+        <title>Nico Gelato - Portafolio</title>
+        <meta property="og:title" content="Nico Gelato - Portafolio" />
+        <meta property="og:site_name" content="Nico Gelato - Portafolio" />
+        <meta
+          name="description"
+          content="Portafolio de desarrollo web de Nico Gelato"
+        />
+        <meta
+          property="og:description"
+          content="Portafolio de desarrollo web de Nico Gelato"
+        />
+        <meta
+          name="keywords"
+          content="React, Javascript, Node, Python, Css, Html, Gatsby, Bootstrap, Material ui, Wordpress, Frontend, Backend, Desarrollo, Web, Portafolio, Nico Gelato, Portfolio, Developer"
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="es_AR" />
+        <meta property="og:locale:alternate" content="es_LA" />
+        <meta property="og:url" content="http://nicogelato.gatsbyjs.io" />
+        <link rel="canonical" href="http://nicogelato.gatsbyjs.io" />
+      </Helmet>
       <ThemeProvider theme={darkTheme}>
         <CssBaseline />
         <Header />
